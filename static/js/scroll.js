61fbbf89479c0b849367b4b062c31d8d9190fbf6
yiyun <128479933+wonder0091@users.noreply.github.com>
2024-07-04 15:22:12 +0800

    Update scroll.js

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -8,7 +8,7 @@
         let tip = `
             <div id="dialog-overlay" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); display: flex; align-items: center; justify-content: center;">
                 <div style="width: 80%; max-width: 300px; height: 200px; background: white; padding: 20px; border-radius: 10px; box-shadow: 0 2px 10px rgba(0,0,0,0.1); display: flex; flex-direction: column; justify-content: space-between;">
-                    <p style="text-align: center; margin-bottom: 20px;font-weight:bold">请确认是否成年，未成年禁止进入</p>
+                    <p style="text-align: center; margin-bottom: 20px;font-weight:bold">请确认是否已满18岁，未成年禁止进入</p>
                     <div style="display: flex; justify-content: space-between;">
                         <button id="yesButton" style="background: #4CAF50; color: white; border: none; padding: 10px 20px; border-radius: 5px;">是</button>
                         <button id="noButton" style="background: #f44336; color: white; border: none; padding: 10px 20px; border-radius: 5px;">否</button>
