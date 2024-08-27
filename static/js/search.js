92258b123093ee518a3c7cdc2b52b71ba9471e68
yiyun <128479933+wonder0091@users.noreply.github.com>
2024-08-27 14:38:29 +0800

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -1,4 +1,10 @@
- function performSearch() {
+document.addEventListener("DOMContentLoaded", function() {
+    function performSearch(event) {
+
+        if (event) {
+            event.preventDefault();
+        }
+
         var searchEngineSelect = document.getElementById("search-engine");
         var selectedEngine = searchEngineSelect.options[searchEngineSelect.selectedIndex].value;
         var searchInputValue = document.getElementById("search-input").value;
@@ -8,32 +14,7 @@
         window.open(searchUrl, '_blank');
     }
 
-    // Add event listener for form submission
-    document.getElementById("search-form").addEventListener("submit", function (event) {
-        // Prevent the default form submission behavior
-        event.preventDefault();
+    document.querySelector('button').addEventListener("click", performSearch);
 
-        // Perform the search when the form is submitted
-        performSearch();
-    });
-
-
-
- function performSearch(event) {
-        var searchEngineSelect = document.getElementById("search-engine");
-        var selectedEngine = searchEngineSelect.options[searchEngineSelect.selectedIndex].value;
-        var searchInputValue = document.getElementById("search-input").value;
-
-        var searchUrl = selectedEngine.replace('%s', encodeURIComponent(searchInputValue));
-
-        window.open(searchUrl, '_blank');
-    }
-
-    // Add event listener for form submission
-    document.getElementById("search-form").addEventListener("submit", function (event) {
-        // Prevent the default form submission behavior
-        event.preventDefault();
-
-        // Perform the search when the form is submitted
-        performSearch();
-    });
+    document.getElementById("search-form").addEventListener("submit", performSearch);
+});
