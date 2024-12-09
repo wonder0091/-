01d035e08456d61e7ed15eee4c4a0ff7247be514
yy <128479933+wonder0091@users.noreply.github.com>
2024-12-09 19:12:49 +0800

    Update scroll.js

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,11 +1,19 @@
+
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
+    const rightContainer = document.getElementById('right-container');
     if (sectionId === 'anime') {
-        const rightContainer = document.getElementById('right-container');
-        rightContainer.scrollTop = 0; // 将right-container滚动到顶部
+        rightContainer.scrollTop = 0; 
         return;
-    } else {
-        section.scrollIntoView({ behavior: 'smooth' });
+    }else{
+        if (section) {
+            const offsetTop = section.offsetTop - rightContainer.offsetTop; 
+            rightContainer.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth', 
+            });
+        }
     }
+
 }
 
